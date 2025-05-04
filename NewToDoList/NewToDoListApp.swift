1c2c79307b6897f2e15ca8961d3d8d41a328b046
Christa Kulanda <christakulanda@gmail.com>
2025-05-04 17:00:48 -0500

    Initial Commit

---
+++ NewToDoList/NewToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewToDoListApp.swift
+//  NewToDoList
+//
+//  Created by Scholar on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct NewToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewToDoListTests/NewToDoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewToDoListTests.swift
+//  NewToDoListTests
+//
+//  Created by Scholar on 5/4/25.
+//
+
+import Testing
+@testable import NewToDoList
+
+struct NewToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
