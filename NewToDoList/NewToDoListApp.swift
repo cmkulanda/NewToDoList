39ced30396c5c1ee4ccf8e4421780e6e9a551fd1
Christa Kulanda <christakulanda@gmail.com>
2025-05-04 19:02:57 -0500

    iteration one part 1

--- NewToDoList/NewToDoListApp.swift
+++ NewToDoList/NewToDoListApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct NewToDoListApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: ToDoItem.self)
         }
     }
 }
